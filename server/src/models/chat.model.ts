e136b4affa0d7bf33b7f9452a1b511c72353d8c2
hoangtrung1002 <n.jordan1002@gmail.com>
2025-11-01 23:11:55 +0700

    feat: create User, Chat, Message models

---
+++ server/src/models/chat.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface ChatDocument extends Document {
+  participants: mongoose.Types.ObjectId[];
+  lastMessage: mongoose.Types.ObjectId;
+  isGroup: boolean;
+  groupName: string;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const chatSchema = new Schema<ChatDocument>(
+  {
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+    isGroup: { type: Boolean, default: false },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+const ChatModel =
+  mongoose.models.Chat || mongoose.model<ChatDocument>("Chat", chatSchema);
+export default ChatModel;
