0853f05fbe5a230dc488fd02e618e548de66d036
hoangtrung1002 <n.jordan1002@gmail.com>
2025-11-09 20:02:37 +0700

    feat&ui: store states in zustand, sign in & singn up page, authentication functionalities

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+import { useAuth } from "./hooks/use-auth";
 import AppRoutes from "./routes";
+import Logo from "./components/logo";
+import { Spinner } from "./components/ui/spinner";
+import { useLocation } from "react-router-dom";
+import { isAuthRoute } from "./routes/routes";
 
 function App() {
+  const { pathname } = useLocation();
+  const { user, isAuthStatusLoading, isAuthStatus } = useAuth();
+  const isAuth = isAuthRoute(pathname);
+
+  useEffect(() => {
+    isAuthStatus();
+  }, [isAuthStatus]);
+
+  if (isAuthStatusLoading && !user && !isAuth)
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Logo imgClass="size-20" showText={false} />
+        <Spinner className="w-6 h-6" />
+      </div>
+    );
+
   return <AppRoutes />;
 }
 
--- client/src/routes/route-guard.tsx
+++ client/src/routes/route-guard.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useAuth } from "@/hooks/use-auth";
+import { Navigate, Outlet } from "react-router-dom";
 
 interface Props {
   requireAuth?: boolean;
 }
 
 const RouteGuard = ({ requireAuth }: Props) => {
-  console.log(requireAuth);
+  const { user } = useAuth();
+  if (requireAuth && !user) return <Navigate to="/" replace />;
+  // ex: If the user goes to /sign-up while already logged in → redirect to /chat.
+  if (!requireAuth && user) return <Navigate to="/chat" replace />;
 
   return <Outlet />;
 };
